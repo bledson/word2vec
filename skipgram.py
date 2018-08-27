92a56039f93624a118b7a6d6c8d14bf5e1b39212
Bledson Bezerra <bledson@gmail.com>
2018-08-27 00:15:28 -0300

    minor fixes; more informative models' names

--- skipgram.py
+++ skipgram.py
@@ -1,29 +1,32 @@
 import math
-import numpy as np
 import random
 import re
-import tensorflow as tf
 from collections import Counter
 from itertools import chain
 
+import numpy as np
+import tensorflow as tf
+
 DATA_FILENAME = 'macmorpho-train.txt'
 BATCH_SIZE = 128
-EPOCHS = 40
+EPOCHS = 100
 STEP = 40000
-LEARNING_RATE = 1.
+LEARNING_RATE = .25
 MIN_COUNT = 5
+MIN_LENGTH = 5
 WINDOW_SIZE = 2
-NUM_SAMPLED = 64
-EMBEDDING_SIZE = 100
+NUM_SAMPLED = 128
+EMBEDDING_SIZE = 300
 SAMPLING_FACTOR = 1e-5
 SMOOTH_FACTOR = .75
 
 
-def skipgrams(sequence,
-              window_size=5,
-              shuffle=True,
-              sampling_table=None,
-              seed=None):
+def skipgrams(
+        sequence,
+        window_size=5,
+        shuffle=True,
+        sampling_table=None,
+        seed=None):
     words = []
     contexts = []
     for i, wi in enumerate(sequence):
@@ -59,10 +62,10 @@ def prepare_data(filename):
         num = re.compile(r'_.+')
         sentences = [[re.sub(num, '', token).lower() for token in line.split()]
                      for line in texts]
-    senteces = list(filter( lambda sent: len(sent) >= MIN_LENGTH , sentences))  
-        
+        sentences = [sent for sent in sentences if len(sent) > MIN_LENGTH]
+
     words_freqs = Counter(chain.from_iterable(sentences)).most_common()
-    words_freqs.append(('unk', MIN_COUNT + 1))
+    words_freqs.append(('<unk>', MIN_COUNT + 1))
     words, frequencies = zip(*[x for x in words_freqs if x[1] > MIN_COUNT])
     word2id = {w: i for i, w in enumerate(words)}
     id2word = [key for key, val in word2id.items()]
@@ -71,7 +74,7 @@ def prepare_data(filename):
 
 
 def texts_to_sequences(texts, word2id):
-    return [[word2id[token] if token in word2id else word2id['unk']
+    return [[word2id[token] if token in word2id else word2id['<unk>']
              for token in line] for line in texts]
 
 
@@ -104,9 +107,8 @@ with tf.Graph().as_default() as graph:
     with tf.name_scope('data'):
         dataset = tf.data.Dataset.from_tensor_slices(
             (np.asarray(words, np.int64),
-             np.asarray(contexts, np.int64))).repeat(EPOCHS)
-        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(
-            BATCH_SIZE))
+             np.asarray(contexts, np.int64)))
+        dataset = dataset.batch(BATCH_SIZE, drop_remainder=True).repeat(EPOCHS)
         iterator = dataset.make_initializable_iterator()
         inputs, labels = iterator.get_next()
         labels_matrix = tf.reshape(labels, [BATCH_SIZE, 1])
@@ -117,6 +119,7 @@ with tf.Graph().as_default() as graph:
             shape=[len(w2id), EMBEDDING_SIZE],
             dtype=tf.float32,
             initializer=tf.glorot_uniform_initializer())
+        tf.assign(embeddings[0], tf.zeros(EMBEDDING_SIZE))
         embs = tf.nn.embedding_lookup(embeddings, inputs)
 
     with tf.name_scope('weights'):
@@ -156,11 +159,11 @@ with tf.Graph().as_default() as graph:
             dtype=tf.int32,
             name='global_step',
             trainable=False)
-        optimizer = tf.train.MomentumOptimizer(.5, .9).minimize(
+        optimizer = tf.train.MomentumOptimizer(LEARNING_RATE, .9).minimize(
             loss,
             global_step=global_step)
 
-    saver = tf.train.Saver(max_to_keep=1, name='saver')
+    saver = tf.train.Saver(name='saver', max_to_keep=1)
     with tf.Session(graph=graph) as sess:
         sess.run(iterator.initializer)
         sess.run(tf.global_variables_initializer())
@@ -175,12 +178,20 @@ with tf.Graph().as_default() as graph:
                 loss_batch, _ = sess.run([loss, optimizer])
                 average_loss += loss_batch
                 if step % STEP == 0:
-                    saver.save(sess, './model', global_step)
+                    saver.save(
+                        sess,
+                        './models/skip-gram_{}d_{}n'.format(EMBEDDING_SIZE,
+                                                            NUM_SAMPLED),
+                        global_step)
                     print('Average loss at step {}: {:5.4f}'.format(
                         step, average_loss / STEP))
                     average_loss = 0.
             except tf.errors.OutOfRangeError:
-                saver.save(sess, './model', global_step)
+                saver.save(
+                    sess,
+                    './models/skip-gram_{}d_{}n'.format(EMBEDDING_SIZE,
+                                                        NUM_SAMPLED),
+                    global_step)
                 break
 
         writer.close()
